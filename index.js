1afa0155f32a4662913702f5cbf135656bb8f5e9
Brian Woodward <brian.woodward@gmail.com>
2014-12-22 13:36:36 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * stringify-keys <https://github.com/doowb/stringify-keys>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
