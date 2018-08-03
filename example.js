9c58f85f120c92f2524538bd430e0c3f1bbbcff1
doowb <brian.woodward@gmail.com>
2018-08-03 14:09:43 -0400

    update example and docs run verb to generate readme documentation

--- example.js
+++ example.js
@@ -15,3 +15,7 @@ console.log(stringify(obj, { separator: '/' }));
 var obj = { 'a.b.c': { d: 'e' } };
 console.log(stringify(obj, { escape: str => str.split('.').join('/') }));
 //=>  [ 'a/b/c.d', 'a/b/c' ]
+
+var obj = { a: 'a', b: [{ c: { d: 'e' } }, { f: { g: 'h' } }] };
+console.log(stringify(obj));
+//=> [ 'a', 'b.0.c.d', 'b.0.c', 'b.0', 'b.1.f.g', 'b.1.f', 'b.1', 'b' ]
